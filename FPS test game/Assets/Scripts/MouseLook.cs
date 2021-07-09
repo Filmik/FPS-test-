7501cd64a82b300dfca389bee58a679f53cdd8d5
Filmik <36886370+Filmik@users.noreply.github.com>
2021-07-09 10:56:10 +0200

    Utworzenie projektu, Player Controller, Kamera, poruszanie się

---
+++ FPS test game/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class MouseLook : MonoBehaviour
+{
+    //[SerializeField]
+    //float mouseSensitivity = 100f;
+    //[SerializeField]
+    //Transform playerBody;
+    //float mouseX;
+    //float mouseY;
+    //PlayerInput input;
+    //void Update()
+    //{
+    //    //mouseX= input.PlayerControls.MouseX.ReadValue();
+    //    //mouseY= input.PlayerControls.MouseY.ReadValue();
+    //    //mouseSensitivity * Time.deltaTime;
+    //    playerBody.Rotate(Vector3.up * mouseX);
+    //}
+
+    //public void OnLook(InputAction.CallbackContext value)
+    //{
+    //   // Vector2 mouseX = value.ReadValue<Vector2>();
+
+    //}
+}
---
+++ FPS test game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    Camera mainCamera;
+    [SerializeField]
+    float movementSpeed = 1;
+    [SerializeField]
+    float jumpForce = 5;
+    [SerializeField]
+    float mouseSensitivity = 100f;
+    [SerializeField]
+    float clampYMin = -90;
+    [SerializeField]
+    float clampYMax = 90;
+    float xRotation;
+    Vector2 inputMovement;
+    Vector3 move;
+    CharacterController controller;
+    //PlayerAnimationController animationController;
+    Rigidbody _rigidbody;
+    Transform playerTransform;
+    void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        controller = GetComponent<CharacterController>();
+        playerTransform = transform;
+       // animationController = GetComponent<PlayerAnimationController>();
+    }
+
+    void Update()
+    {
+        if (inputMovement != Vector2.zero)
+        {
+            move = transform.right * inputMovement.x + transform.forward * inputMovement.y;
+            controller.Move(move * movementSpeed * Time.deltaTime);
+        }
+    }
+    public void OnMouse(InputAction.CallbackContext value)//On mouse movement
+    {
+        Vector2 mousePos = value.ReadValue<Vector2>();
+        float mouseX = mousePos.x * mouseSensitivity * Time.deltaTime;
+        float mouseY = mousePos.y * mouseSensitivity * Time.deltaTime;
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation,clampYMin,clampYMax);
+        mainCamera.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerTransform.Rotate(Vector3.up * mouseX);
+    }
+
+    public void OnMovement(InputAction.CallbackContext value)
+    {
+        inputMovement = value.ReadValue<Vector2>();
+        //if (runMovement != 0)
+        //    animationController.playerRun();
+        //else
+        //    animationController.playerStopRuning();
+
+    }
+
+    //public void OnAttack(InputAction.CallbackContext value)
+    //{
+    //    if (value.started)
+    //    {
+    //        animationController.playerAttack();
+    //    }
+    //}
+    //public void OnJump(InputAction.CallbackContext value)
+    //{
+    //    if (Mathf.Abs(_rigidbody2D.velocity.y) < 0.001f)
+    //    {
+    //        _rigidbody2D.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+    //        animationController.playerJumpUp(true);
+    //    }
+    //}
+}
